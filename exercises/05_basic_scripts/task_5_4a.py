942372f2aec20f8851fce893769e0aae126897fc
Serhii Protsenko <shaytanbat@gmail.com>
2023-04-01 16:52:49 +0300

    Comleted and checked the task 5.4a

--- exercises/05_basic_scripts/task_5_4a.py
+++ exercises/05_basic_scripts/task_5_4a.py
@@ -49,3 +49,23 @@ Mask:
 255       255       255       240
 11111111  11111111  11111111  11110000
 """
+ip_str, mask_str = input('Enter network address: ').split()
+
+ip_octets = ip_str.split('.')
+ip_oct_1, ip_oct_2, ip_oct_3, ip_oct_4 = [int(i) for i in ip_octets]
+
+mask_octets = mask_str.split('.')
+mask_oct_1, mask_oct_2, mask_oct_3, mask_oct_4 = [int(i) for i in mask_octets]
+netmask = sum(bin(int(x)).count('1') for x in mask_octets)
+
+print(f'''
+Network:
+{ip_oct_1:<8}  {ip_oct_2:<8}  {ip_oct_3:<8}  {ip_oct_4:<8}
+{ip_oct_1:08b}  {ip_oct_2:08b}  {ip_oct_3:08b}  {ip_oct_4:08b}''')
+
+print(f'''
+Mask:
+/{netmask}
+{mask_oct_1:<8}  {mask_oct_2:<8}  {mask_oct_3:<8}  {mask_oct_4:<8}
+{mask_oct_1:08b}  {mask_oct_2:08b}  {mask_oct_3:08b}  {mask_oct_4:08b}''')
+
