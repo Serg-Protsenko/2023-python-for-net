0842335560f12945633bb44baaf6e251692e5c25
Serhii Protsenko <shaytanbat@gmail.com>
2023-04-07 22:11:12 +0300

    Comleted and checked the task 5.4b

--- exercises/05_basic_scripts/task_5_4b.py
+++ exercises/05_basic_scripts/task_5_4b.py
@@ -14,7 +14,7 @@
 * 10.0.1.1 255.255.255.0 - хост із мережі 10.0.1.0 255.255.255.0
 * 10.0.5.195 255.255.255.240 - хост із мережі 10.0.5.192 255.255.255.240
 
-Приклад роботи завдання якщо користувач ввів адресу 10.0.1.1 255.255.255.0,
+Приклад роботи завдання якщо користувач ввів адресу 10.0.1.1 255.255.255.0
 
 Network:
 10        0         1         0
@@ -42,4 +42,43 @@ bin_ip = "0000101000000001000000111000011"
 А адреса мережі буде перших 28 символів з bin_ip + 0000 (4 тому що всього в
 адресі може бути 32 біти, а 32 - 28 = 4)
 00001010000000010000000111000000
-""
+
+#  10.0.1.1 255.255.255.0
+"""
+
+ip_str, mask_str = input('Enter network address: ').split()
+
+mask_octets = mask_str.split('.')
+mask_oct_1, mask_oct_2, mask_oct_3, mask_oct_4 = [int(i) for i in mask_octets]
+mask_bin_oct_1, mask_bin_oct_2, mask_bin_oct_3, mask_bin_oct_4 = [int(bin(int(i))[2:]) for i in mask_octets]
+mask_list_octs = [bin(int(i)) for i in mask_octets]
+
+netmask = sum(bin(int(x)).count('1') for x in mask_octets)
+netmask_bin = [bin(int(x)) for x in mask_octets]
+
+ip_octets = ip_str.split('.')
+
+mask_bin_octs = [format(int(octet), '08b') for octet in mask_octets]
+ip_bin_octs = [format(int(octet), '08b') for octet in ip_octets]
+
+bin_mask = "".join(mask_bin_octs)
+bin_ip = "".join(ip_bin_octs)
+
+net_addr = int(bin_mask, 2) & int(bin_ip, 2)
+net_addr_bin = bin(net_addr)[2:].zfill(32)
+
+net_addr_octs = [net_addr_bin[i:i+8] for i in range(0, 32, 8)]
+net_addr_str = [int(octet, 2) for octet in net_addr_octs]
+ip_oct_1, ip_oct_2, ip_oct_3, ip_oct_4 = net_addr_str
+
+
+print(f'''
+Network:
+{ip_oct_1:<8}  {ip_oct_2:<8}  {ip_oct_3:<8}  {ip_oct_4:<8}
+{ip_oct_1:08b}  {ip_oct_2:08b}  {ip_oct_3:08b}  {ip_oct_4:08b}''')
+
+print(f'''
+Mask:
+/{netmask}
+{mask_oct_1:<8}  {mask_oct_2:<8}  {mask_oct_3:<8}  {mask_oct_4:<8}
+{mask_oct_1:08b}  {mask_oct_2:08b}  {mask_oct_3:08b}  {mask_oct_4:08b}''')
