8e4dec4654ca052a85e27635067d69c902cfe121
Serhii Protsenko <shaytanbat@gmail.com>
2023-04-10 15:29:52 +0300

    Comleted and checked the task 6.6b

--- exercises/06_control_structures/task_6_6b.py
+++ exercises/06_control_structures/task_6_6b.py
@@ -34,3 +34,34 @@ Enter IP address: 255.255.255.255
 local broadcast
 
 """
+
+while True:
+    ip_addr = input('Enter IP address: ')
+    ip_octs = ip_addr.split('.')
+
+    if not all([i.isdigit() for i in ip_octs]) or len(ip_octs) != 4:
+        print('Wrong IP address')
+        continue
+    elif ip_addr.count('.') != 3:
+        print('Wrong IP address')
+        continue
+    elif not all([0 <= int(i) <= 255 for i in ip_octs]):
+        print('Wrong IP address')
+        continue
+    else:
+        ip_first_oct = int(ip_addr.split('.')[0])
+        if 1 <= ip_first_oct <= 223:
+            print('unicast')
+            break
+        elif 224 <= ip_first_oct <= 239:
+            print('multicast')
+            break
+        elif  ip_first_oct == 255:
+            print('local broadcast')
+            break
+        elif  ip_first_oct == 0:
+            print('unassigned')
+            break
+        else:
+            print('unused')
+            break
