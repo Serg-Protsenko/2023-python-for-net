7aa0ec6b6b40a5e1c179aa0c0dd56572b6e8a1d5
Serhii Protsenko <shaytanbat@gmail.com>
2023-04-10 15:22:14 +0300

    Comleted and checked the task 6.6a

--- exercises/06_control_structures/task_6_6a.py
+++ exercises/06_control_structures/task_6_6a.py
@@ -46,3 +46,28 @@ $ python task_6_6a.py
 Enter IP address: 50.1.1.1.1
 Wrong IP address
 """
+
+ip_addr = input('Enter IP address: ')
+ip_octs = ip_addr.split('.')
+# print(ip_octs)
+# print(all([0 <= int(i) <= 255 for i in ip_octs]))
+
+
+if not all([i.isdigit() for i in ip_octs]) or len(ip_octs) != 4:
+    print('Wrong IP address')
+elif ip_addr.count('.') != 3:
+    print('Wrong IP address')
+elif not all([0 <= int(i) <= 255 for i in ip_octs]):
+    print('Wrong IP address')
+else:
+    ip_first_oct = int(ip_addr.split('.')[0])
+    if 1 <= ip_first_oct <= 223:
+        print('unicast')
+    elif 224 <= ip_first_oct <= 239:
+        print('multicast')
+    elif  ip_first_oct == 255:
+        print('local broadcast')
+    elif  ip_first_oct == 0:
+        print('unassigned') 
+    else:
+        print('unused')
