cd9d9e0fc592b6cca21d28576c9de4c5946a38a1
Serhii Protsenko <shaytanbat@gmail.com>
2023-04-13 14:07:55 +0300

    Compled and check the task 7.1

--- exercises/07_files/task_7_1.py
+++ exercises/07_files/task_7_1.py
@@ -12,3 +12,22 @@ Last update           3d18h
 Outbound Interface    FastEthernet0/0
 
 """
+
+ip_info = {}
+
+with open('ospf.txt') as f:
+    for line in f:
+        line_list = line.rstrip().split()
+        prefix = line_list[1]
+        metric = line_list[2].strip('[]')
+        next_hop = line_list[4].strip(',')
+        update = line_list[5].strip(',')
+        interface = line_list[-1]
+        
+        sample = f'''
+Prefix                {prefix}
+AD/Metric             {metric}
+Next-Hop              {next_hop}
+Last update           {update}
+Outbound Interface    {interface}'''
+        print(sample)
