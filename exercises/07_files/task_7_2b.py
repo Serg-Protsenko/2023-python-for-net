82beb3dd18275bc5899680345cbf0972bf5b76b7
Serhii Protsenko <shaytanbat@gmail.com>
2023-04-13 15:38:36 +0300

    Compled and check the task 7.2b

--- exercises/07_files/task_7_2b.py
+++ exercises/07_files/task_7_2b.py
@@ -15,4 +15,26 @@ $ python task_7_2b.py config_sw1.txt new_config.txt
 ignore та рядки, що починаються на '!'.
 """
 
+from sys import argv, exit
+
 ignore = ["duplex", "alias", "configuration", "end", "service"]
+
+if len(argv) > 2:
+    file_read = argv[1]
+    file_write = argv[2]
+
+else:
+    print("Потрібно передати ім'я файлу для зчитування і для запису, як аргумент командного рядка.")
+    exit(1)
+
+try:
+    with open(file_read, 'r') as file, open(file_write, 'w') as new_file:
+        for line in file:
+            if line.startswith('!') or any(i for i in ignore if i in line):
+                continue
+            new_file.write(line)
+
+except FileNotFoundError:
+    print(f"Файл '{file_read}' не знайдено.")
+except Exception as e:
+    print(f"Помилка при обробці файлу: {e}")
