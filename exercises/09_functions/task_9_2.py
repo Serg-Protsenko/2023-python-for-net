a605e3db4d6f8d56ecb85adab18fc7e2ca0d1f18
Serhii Protsenko <shaytanbat@gmail.com>
2023-04-21 19:43:34 +0300

    Completed and checked task 9.1

--- exercises/09_functions/task_9_2.py
+++ exercises/09_functions/task_9_2.py
@@ -50,3 +50,18 @@ False
 """
 
 ip_list = ["10.1.1.1", "10.3.a.a", "500.1.1.1", "150.168.100.1", "62.150.240.300"]
+
+def check_ip(ip_addr):
+    ip_octs = ip_addr.split('.')
+
+    if not all([i.isdigit() for i in ip_octs]) or len(ip_octs) != 4:
+        return False
+    elif ip_addr.count('.') != 3:
+        return False
+    elif not all([0 <= int(i) <= 255 for i in ip_octs]):
+        return False
+    else:
+        return True
+
+for ip in ip_list:
+    print(check_ip(ip))
